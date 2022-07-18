a6ec972e25cf6dbf15a4e8bfe7acefe738cb92ec
Sebastian Dresbach <sebastiandresbach@sebastians-mbp.wireless.unimaas.local>
2022-07-18 16:25:07 +0200

    update plotting

--- code/plotting.py
+++ code/plotting.py
@@ -298,6 +298,73 @@ for i, modality in enumerate(['BOLD', 'VASO']):
 
     plt.show()
 
+
+
+#########################################################
+########## Plot event FIRs for two individuals ##########
+#########################################################
+
+data = pd.read_csv('../results/FIR_results.csv')
+palettesLayers = {'VASO':['#1f77b4','#7dadd9','#c9e5ff'],
+'BOLD':['#ff7f0e', '#ffae6f','#ffdbc2']}
+
+limits = {'sub-05':16,
+    'sub-08':12,
+    'sub-14':9
+    }
+
+for sub in ['sub-05', 'sub-08', 'sub-14']:
+    for i, modality in enumerate(['BOLD', 'VASO']):
+
+        fig, ax = plt.subplots(figsize=(7,5))
+
+        tmp = data.loc[(data['subject']==sub)&(data['modality']==modality)&(data['focus']=='v1')]
+
+        sns.lineplot(ax=ax, data=tmp , x="volume", y="data", hue='layer',palette=palettesLayers[modality],linewidth=2)
+
+
+        yLimits = ax.get_ylim()
+        ax.set_ylim(-2,limits[sub])
+        ax.set_yticks(range(-2,limits[sub]+1,2),fontsize=18)
+
+        # prepare x-ticks
+        ticks = range(1,12,2)
+        labels = (np.arange(0,11,2)*1.3).round(decimals=1)
+        for k,label in enumerate(labels):
+            if (label - int(label) == 0):
+                labels[k] = int(label)
+
+        ax.yaxis.set_tick_params(labelsize=tickLabelSize)
+        ax.xaxis.set_tick_params(labelsize=tickLabelSize)
+
+        if i == 0:
+            ax.set_ylabel(r'Signal [$\beta$]', fontsize=labelSize)
+        else:
+            ax.set_ylabel(r'', fontsize=labelSize)
+            ax.set_yticks([])
+
+        ax.legend(loc='upper right',fontsize=tickLabelSize)
+
+        # tweak x-axis
+        ax.set_xticks(ticks)
+        ax.set_xticklabels(labels,
+            fontsize=tickLabelSize)
+        ax.set_xlabel('Time [s]',
+            fontsize=labelSize)
+        ax.set_title(modality, fontsize=36)
+
+        # draw lines
+        ax.axvspan(1, 3/1.3, color='#e5e5e5', alpha=0.2, lw=0, label = 'stimulation on')
+        ax.axhline(0,linestyle='--',color='white')
+
+        # ax.axvline((np.arange(0,11)*1.3).round(decimals=1)[3],linestyle='--',color='white')
+
+        plt.savefig(f'../results/{sub}_v1_{modality}_eventResults_withLayers.png', bbox_inches = "tight")
+
+
+        plt.show()
+
+
 #########################################
 ########## Plot event profiles ##########
 #########################################
