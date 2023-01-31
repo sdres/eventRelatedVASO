d2e7066c8305a5add590387ae8444e1fc47c7af1
sdres <sebastian.dresbach@gmail.com>
2023-01-31 11:37:52 +0100

    Cosmetics

--- code/02_mocoCoregistration.py
+++ code/02_mocoCoregistration.py
@@ -7,13 +7,10 @@ Do motion correction and register runs
 import ants
 import os
 import glob
-from nipype.interfaces import afni
 import nibabel as nb
 import numpy as np
 import subprocess
-from IPython.display import clear_output
 import nipype.interfaces.fsl as fsl
-import itertools
 import pandas as pd
 import sys
 
@@ -26,14 +23,12 @@ ROOT = '/Users/sebastiandresbach/data/eventRelatedVASO/Nifti'
 
 # Set subjects to work on
 SUBS = ['sub-08','sub-09','sub-11','sub-12','sub-13','sub-14']
-SUBS = ['sub-07']
-# SUBS = ['sub-06','sub-07','sub-08','sub-09','sub-11','sub-12','sub-13','sub-14']
 
 for sub in SUBS:
     print(f'Working on {sub}')
 
     # Set folder for subject
-    subFolder = f'{ROOT}/derivativesTest/{sub}'
+    subFolder = f'{ROOT}/derivativesTestTest/{sub}'
     # Create folder if it does not exist
     if not os.path.exists(subFolder):
         os.makedirs(subFolder)
@@ -64,7 +59,7 @@ for sub in SUBS:
         runs = sorted(glob.glob(f'{ROOT}/{sub}/{ses}/func/{sub}_{ses}_task-*run-00*_cbv.nii.gz'))
 
         # Set folder for session outputs
-        outFolder = f'{ROOT}/derivativesTest/{sub}/{ses}'
+        outFolder = f'{ROOT}/derivativesTestTest/{sub}/{ses}/func'
         # Create folder if it does not exist
         if not os.path.exists(outFolder):
             os.makedirs(outFolder)
@@ -140,13 +135,17 @@ for sub in SUBS:
             # =========================================================================
             # Compute T1w image in EPI space within run
 
-            t1w = computeT1w(f'{outFolder}/{base}_nulled_moco.nii', f'{outFolder}/{base}_notnulled_moco.nii')
+            t1w = computeT1w(f'{outFolder}/{base}_nulled_moco.nii',
+                             f'{outFolder}/{base}_notnulled_moco.nii'
+                             )
+
+            # Get header and affine
             header = nb.load(f'{outFolder}/{base}_nulled_moco.nii').header
             affine = nb.load(f'{outFolder}/{base}_nulled_moco.nii').affine
-            # And save the image
-            img = nb.Nifti1Image(t1w, header=header, affine=affine)
-            nb.save(img, f'{outFolder}/{base}_T1w.nii')
 
+            # And save the image
+            img = nb.Nifti1Image(t1w, header = header, affine = affine)
+            nb.save(img, f'{outFolder}/{base}_T1w.nii')
 
 
 
@@ -158,7 +157,7 @@ for sub in SUBS:
     print(f'Working on {sub}')
 
     # Set folder for subject
-    subFolder = f'{ROOT}/derivativesTest/{sub}'
+    subFolder = f'{ROOT}/derivativesTestTest/{sub}'
 
     # =========================================================================
     # Look for sessions
@@ -182,14 +181,13 @@ for sub in SUBS:
         print(f'Working on {ses}')
 
         # Set folder for session outputs
-        outFolder = f'{ROOT}/derivativesTest/{sub}/{ses}'
+        outFolder = f'{ROOT}/derivativesTestTest/{sub}/{ses}/func'
 
         # Set folder for motion traces
         motionDir = f'{outFolder}/motionParameters'
 
         # Collectall runs within session (containing both nulled and notnulled images)
         runs = sorted(glob.glob(f'{ROOT}/{sub}/{ses}/func/{sub}_*_task-*run-00*_cbv.nii.gz'))
-        print(f'Found {runs}')
 
         if not sub == 'sub-07':
             try: # trying to register to first eventStim run
@@ -216,36 +214,44 @@ for sub in SUBS:
                     refBase = os.path.basename(referenceRun).rsplit('.', 2)[0][:-4]
                     print(f'Registering eventStim runs to {refBase}')
                     print('Continuing...')
-                    continue
+                    continue  # Skip this run, as it is the reference
 
                 if 'blockStim_run-001' in run:
                     referenceRun = run
                     refBase = os.path.basename(referenceRun).rsplit('.', 2)[0][:-4]
                     print(f'Registering blockStim runs to {refBase}')
                     print('Continuing...')
-                    continue
+                    continue  # Skip this run, as it is the reference
 
 
             print(f'Registering {base} to {refBase}')
 
             fixed = ants.image_read(f'{outFolder}/{refBase}_T1w.nii')
-            mask = ants.image_read(f'{outFolder}/{refBase}_nulled_moma.nii')
-
-
+            # Get motion mask
+            mask = ants.image_read(f'{outFolder}/{refBase}_nulled_moma.nii.gz')
             # Define moving image
             moving = ants.image_read(f'{outFolder}/{base}_T1w.nii')
             # Do registration
-            mytx = ants.registration(fixed=fixed, moving=moving, type_of_transform='Rigid', mask=mask)
+            mytx = ants.registration(fixed = fixed,
+                                     moving = moving,
+                                     type_of_transform = 'Rigid',
+                                     mask = mask
+                                     )
+
             # Copy transform file for later
             os.system(f"cp {mytx['fwdtransforms'][0]} {outFolder}/{base}_T1w_registered-{refBase}.mat")
+
             # Apply registration
-            mywarpedimage = ants.apply_transforms(fixed=fixed, moving=moving, transformlist=mytx['fwdtransforms'], interpolator='bSpline')
+            mywarpedimage = ants.apply_transforms(fixed = fixed,
+                                                  moving=moving,
+                                                  transformlist = mytx['fwdtransforms'],
+                                                  interpolator = 'bSpline'
+                                                  )
             # Save transformed image
             ants.image_write(mywarpedimage, f'{outFolder}/{base}_T1w_registered-{refBase}.nii')
 
-
             # Load transform we just saved
-            transform1 = f'{outFolder}/{base}_T1w_registered-{refBase}.mat'
+            transformBetween = f'{outFolder}/{base}_T1w_registered-{refBase}.mat'
 
             # Loop over nulled and notnulled
             for start, modality in enumerate(['notnulled', 'nulled']):
@@ -269,7 +275,7 @@ for sub in SUBS:
                     else:
                         vol = data[:,:,:,i]
 
-                    img = nb.Nifti1Image(vol, header=header, affine=affine)
+                    img = nb.Nifti1Image(vol, header = header, affine = affine)
                     nb.save(img, f'{outFolder}/{base}_{modality}_vol{i:03d}.nii')
 
 
@@ -277,19 +283,23 @@ for sub in SUBS:
                     # Load volume
                     moving = ants.image_read(f'{outFolder}/{base}_{modality}_vol{i:03d}.nii')
                     # Load within run transofrmation of that volume
-                    transform2 = f'{motionDir}/{base}/{base}_{modality}_vol{i:03d}.mat'
+                    transformWithin = f'{motionDir}/{base}/{base}_{modality}_vol{i:03d}.mat'
 
                     # Apply both transformations
                     mywarpedimage = ants.apply_transforms(fixed = fixed,
                                                           moving = moving,
-                                                          transformlist = [transform1, transform2],
+                                                          transformlist = [transformWithin, transformBetween],
                                                           interpolator = 'bSpline'
                                                           )
                     # Save warped image
                     ants.image_write(mywarpedimage, f'{outFolder}/{base}_{modality}_vol{i:03d}.nii')
 
-                # Make array of data shape to assample warped images
-                newData = np.zeros(data.shape)
+                # Make array of reference-data shape to assample warped images
+                newShape = nb.load(f'{outFolder}/{refBase}_T1w.nii').get_fdata().shape
+                newTimepoints = data.shape[-1]
+                newShape = newShape + (newTimepoints,)
+
+                newData = np.zeros(newShape)
 
                 # Loop over volumes to fill new data
                 for i in range(data.shape[-1]):
@@ -297,48 +307,50 @@ for sub in SUBS:
                     newData[:,:,:,i] = vol
 
                 # Save new data
-                img = nb.Nifti1Image(newData, header=header, affine=affine)
-                nb.save(img, f'{outFolder}/{base}_{modality}_moco_reg.nii')
+                img = nb.Nifti1Image(newData, header = header, affine = affine)
+                nb.save(img, f'{outFolder}/{base}_{modality}_moco.nii')
+
                 # Delete individual volumes
                 os.system(f'rm {outFolder}/{base}_{modality}_vol*.nii')
 
             # =========================================================================
             # Compute T1w image in EPI space within registered run
 
-            t1w = computeT1w(f'{outFolder}/{base}_nulled_moco_reg.nii', f'{outFolder}/{base}_notnulled_moco_reg.nii')
-            header = nb.load(f'{outFolder}/{base}_nulled_moco_reg.nii').header
-            affine = nb.load(f'{outFolder}/{base}_nulled_moco_reg.nii').affine
+            t1w = computeT1w(f'{outFolder}/{base}_nulled_moco.nii',
+                             f'{outFolder}/{base}_notnulled_moco.nii'
+                             )
+
+            header = nb.load(f'{outFolder}/{base}_nulled_moco.nii').header
+            affine = nb.load(f'{outFolder}/{base}_nulled_moco.nii').affine
+
             # And save the image
-            img = nb.Nifti1Image(t1w, header=header, affine=affine)
+            img = nb.Nifti1Image(t1w, header = header, affine = affine)
             nb.save(img, f'{outFolder}/{base}_reg_T1w.nii')
 
 
-# # Make overall t1w
-# dataFiles = sorted(glob.glob(f'{outFolder}/*_moco_reg.nii'))
-# for modality in ['nulled', 'notnulled']:
-#     dataFiles.append(f'{outFolder}/{refBase}_{modality}_moco.nii')
-#
-# for i, file in enumerate(dataFiles):
-#     # Load nulled motion corrected timeseries
-#     nii = nb.load(file)
-#     data = nii.get_fdata()
-#
-#     if i == 0:
-#         combined = data
-#     else:
-#         # Concatenate nulled and notnulled timeseries
-#         combined = np.concatenate((combined,data), axis=3)
-#
-#
-#     stdDev = np.std(combined, axis = 3)
-#
-#     #Compute mean
-#     mean = np.mean(combined, axis = 3)
-#     # Compute variation
-#     cvar = stdDev/mean
-#     # Take inverse
-#     cvarInv = 1/cvar
-#
-# # And save the image
-# img = nb.Nifti1Image(cvarInv, header=header, affine=affine)
-# nb.save(img, f'{outFolder}/{sub}_{ses}_T1w.nii')
+        # Make overall t1w
+        dataFiles = sorted(glob.glob(f'{outFolder}/*_moco.nii'))
+
+        for i, file in enumerate(dataFiles):
+            # Load nulled motion corrected timeseries
+            nii = nb.load(file)
+            data = nii.get_fdata()
+
+            if i == 0:
+                combined = data
+            else:
+                # Concatenate nulled and notnulled timeseries
+                combined = np.concatenate((combined,data), axis=3)
+
+        stdDev = np.std(combined, axis = 3)
+
+        #Compute mean
+        mean = np.mean(combined, axis = 3)
+        # Compute variation
+        cvar = stdDev/mean
+        # Take inverse
+        cvarInv = 1/cvar
+
+        # And save the image
+        img = nb.Nifti1Image(cvarInv, header=header, affine=affine)
+        nb.save(img, f'{outFolder}/{sub}_{ses}_T1w.nii')
